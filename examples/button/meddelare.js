9ffb5ccf0262f707f7dde176a870032130c37579
Joel Purra <code+github@joelpurra.com>
2015-07-09 10:26:50 +0200

    Restructure button example
    - Clean up css
    - Use html5 data attributes
    - Break out script
    
    Closes meddelare/meddelare-examples#2

---
+++ examples/button/meddelare.js
@@ -0,0 +1,24 @@
+(function() {
+    // This code updates Meddelare' social sharing counts for all matching elements on this page.
+    // The url is taken from the elements' data-meddelare-url.
+    //
+    // The request is being made to Meddelare's server (on cloudfront). You need to set up your own server.
+    // See more information on https://meddelare.com/
+    //
+    // Requires jQuery.
+    //
+    // TODO: de-duplicate urls to reduce the number of requests.
+
+    $("[data-meddelare-url]").each(function(index, element) {
+        var $meddelareUrlElement = $(element),
+            url = $meddelareUrlElement.data("meddelare-url");
+
+        $.ajax("https://d12cncu17l9pr5.cloudfront.net/?networks=facebook,twitter,googleplus&url=" + url, {
+            success: function(res, err) {
+                $.each(res, function(network, value) {
+                    $meddelareUrlElement.find("[data-meddelare-network=" + network + "]").attr("data-count", value);
+                });
+            }
+        });
+    });
+}());
